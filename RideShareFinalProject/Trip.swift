bbbdbfbb25da3ec68959cf72a7fb3d1d7c29c6f5
sahand sadri <sahandsadri@mwc-007064.dhcp.missouri.edu>
2017-11-16 13:13:32 -0600

    Commented line for test

--- RideShareFinalProject/Trip.swift
+++ RideShareFinalProject/Trip.swift
@@ -28,7 +28,7 @@ struct driverTrip{
     var returnTime: Date
     var seatsAvailable: Int
     var price: Int
-    //var comments: String
     var comments: String
+    //var comments: String
     
 }
