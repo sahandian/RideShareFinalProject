849ec06d2fef512febe1f4e4356c9c31f4cf5907
Benjarit <bhmcf@mail.missouri.edu>
2017-12-08 03:50:48 -0600

    Implemented: segue to more rows for upcoming trips, finished UI layout for Trip Information, and some others

--- RideShareFinalProject/DashBoardViewController.swift
+++ RideShareFinalProject/DashBoardViewController.swift
@@ -14,26 +14,29 @@ class DashBoardViewController: UIViewController, UITableViewDataSource,UITableVi
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
     func numberOfSections(in tableView: UITableView) -> Int {
         return 2
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return 4
     }
-    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let deque: UITableViewCell
+        var deque: UITableViewCell
         if indexPath.section == 0{
-            deque = tableView.dequeueReusableCell(withIdentifier: "myFirstCell", for: indexPath)
-            if let cell = deque as? DashBoardTableViewCell{
-                cell.infoToBeShown = "Data setted"
+            if indexPath.row == 3{
+                deque = tableView.dequeueReusableCell(withIdentifier: "showMoreCell", for: indexPath)
+            }else{
+                deque = tableView.dequeueReusableCell(withIdentifier: "myFirstCell", for: indexPath)
+                if let cell = deque as? DashBoardTableViewCell{
+                    cell.infoToBeShown = "Data setted"
+                }
             }
         }else{
             deque = tableView.dequeueReusableCell(withIdentifier: "notificationCell", for: indexPath)
@@ -48,11 +51,29 @@ class DashBoardViewController: UIViewController, UITableViewDataSource,UITableVi
         if section == 1{
             return "Notifications"
         }else{
-            return ""
+            return "Upcoming trips"
         }
     }
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 70
+    }
+
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let deleteAction = UIContextualAction(style: .destructive, title:  "Delete", handler: { (ac:UIContextualAction, view:UIView, success:(Bool) -> Void) in
+            print("OK, you have delete the request.")
+            success(true)
+        })
+        return UISwipeActionsConfiguration(actions: [deleteAction])
+    }
+    
+    func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        
+        let confirmAction = UIContextualAction(style: .normal, title:  "Confirm", handler: { (ac:UIContextualAction, view:UIView, success:(Bool) -> Void) in
+            print("OK, you have confirmed the request.")
+            success(true)
+        })
+        confirmAction.backgroundColor = UIColor(red: 0.298, green: 0.851, blue: 0.3922, alpha: 1.0)
+        return UISwipeActionsConfiguration(actions: [confirmAction])
     }
     
     @IBAction func viewPastActionsToggle(_ sender: UISegmentedControl) {
