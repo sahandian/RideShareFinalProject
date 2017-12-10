61fbd2d6f5e5e93bc1ce5afe310b26191ee0c2a5
sahand sadri <sahandsadri@mwc-007097.dhcp.missouri.edu>
2017-12-10 17:30:31 -0600

    Fixed bugs with Ride and Drive

--- RideShareFinalProject/SearchViewController.swift
+++ RideShareFinalProject/SearchViewController.swift
@@ -9,15 +9,14 @@
 import UIKit
 
 class SearchViewController: UIViewController {
-
+    
+  
+    
     @IBOutlet weak var dateField: UITextField!
-    @IBOutlet weak var selectorLabel: UILabel!
     
     let picker = UIDatePicker()
     
-    @IBAction func slider(_ sender: UISlider) {
-        selectorLabel.text = String("\(Int(sender.value)) mi")
-    }
+
     
     func createDatePicker(){
         let toolbar = UIToolbar()
@@ -46,25 +45,26 @@ class SearchViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         createDatePicker()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
+
