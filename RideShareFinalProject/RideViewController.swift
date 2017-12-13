5175621d5d1a736490526dbfd1988e3e10656fa4
sahand sadri <sahandsadri@mwc-007097.dhcp.missouri.edu>
2017-12-13 00:02:03 -0600

    Got the query to pull up the trips from the CloudKit

--- RideShareFinalProject/RideViewController.swift
+++ RideShareFinalProject/RideViewController.swift
@@ -12,6 +12,8 @@ import CloudKit
 class RideViewController: UIViewController {
 
     @IBOutlet weak var tripsTableView: UITableView!
+    
+
     let publicDatabase = CKContainer.default().publicCloudDatabase
     
     var trips = [CKRecord]()
@@ -24,15 +26,52 @@ class RideViewController: UIViewController {
             if let error = error {
                 print(error)
             } else {
+                
+                print("we got this far")
+                //print(trips?.count)
+                
                 self.trips = trips ?? []
+   
+                self.tripsTableView.reloadData()
+                }
             }
-            }}
+            
+        }
     }
     
+    
+    /*
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.trips.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tripsTableView.dequeueReusableCell(withIdentifier: "tripCell", for: indexPath)
+        
+        let trip = trips[indexPath.row]
+        
+        cell.textLabel?.text = trip.object(forKey: "firstName") as? String
+        
+       
+        
+        
+        return cell
+    }
+    */
+  /*  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "showExpense", sender: self)
+    }
+ */
+    
+    override func viewWillAppear(_ animated: Bool) {
+        queryTrips()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+       
+        
         // Do any additional setup after loading the view.
     }
 
@@ -53,3 +92,26 @@ class RideViewController: UIViewController {
     */
 
 }
+
+extension RideViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.trips.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tripsTableView.dequeueReusableCell(withIdentifier: "tripCell", for: indexPath)
+        
+        let trip = trips[indexPath.row]
+        
+        cell.textLabel?.text = trip.object(forKey: "firstName") as? String
+        
+        return cell
+    }
+}
+
+/*extension RideViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "showExpense", sender: self)
+    }
+}*/
+
