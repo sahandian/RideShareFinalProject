813d9f443efa25dcc4ffe6626be3591f49322437
sahand sadri <sahandsadri@mwc-007097.dhcp.missouri.edu>
2017-12-13 04:20:51 -0600

    Got it to pull perfectly

--- RideShareFinalProject/TripViewController.swift
+++ RideShareFinalProject/TripViewController.swift
@@ -28,10 +28,6 @@ class TripViewController: UIViewController {
     @IBOutlet weak var license: UITextField!
     
     
-    
-    
-
-    
     let publicDatabase = CKContainer.default().publicCloudDatabase
     
     var trip: CKRecord?
@@ -49,9 +45,38 @@ class TripViewController: UIViewController {
 
         self.view.backgroundColor = .white
         
-        //firstName.delegate = self
+        firstName.delegate = self
+        lastName.delegate = self
+        email.delegate = self
+        address.delegate = self
+        city.delegate = self
+        state.delegate = self
+        zip.delegate = self
+        address2.delegate = self
+        city2.delegate = self
+        state2.delegate = self
+        zip2.delegate = self
+        departure.delegate = self
+        carMake.delegate = self
+        carModel.delegate = self
+        license.delegate = self
         
         firstName.text = trip?.object(forKey: "firstName") as? String
+        lastName.text = trip?.object(forKey: "lasName") as? String
+        email.text = trip?.object(forKey: "email") as? String
+        address.text = trip?.object(forKey: "address") as? String
+        city.text = trip?.object(forKey: "city") as? String
+        state.text = trip?.object(forKey: "state") as? String
+        zip.text = trip?.object(forKey: "zip") as? String
+        address2.text = trip?.object(forKey: "address2") as? String
+        city2.text = trip?.object(forKey: "city2") as? String
+        state2.text = trip?.object(forKey: "state2") as? String
+        zip2.text = trip?.object(forKey: "zip2") as? String
+        departure.text = trip?.object(forKey: "departure") as? String
+        carModel.text = trip?.object(forKey: "carModel") as? String
+        carMake.text = trip?.object(forKey: "carMake") as? String
+        license.text = trip?.object(forKey: "license") as? String
+        
         // Do any additional setup after loading the view.
     }
 
@@ -74,8 +99,8 @@ class TripViewController: UIViewController {
 }
 
 extension TripViewController: UITextFieldDelegate {
-    func labelShouldReturn(_ label: UILabel) -> Bool {
-        label.resignFirstResponder()
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
         return true
     }
 }
